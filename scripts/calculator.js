dab98e821e0d23795b59b66aef2ead337c7d2a13
kwenkam yemgai <yemgai21@gmail.com>
2024-11-27 15:43:45 +0100

    edits

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -3,26 +3,26 @@ const buttons = document.querySelector('button')
 const specialChars = ['*', '/', '+', '=', '-']
 let output = ''
 
- //Define fuction to calculate based on button clicked.
-const calculate = (btnValue) => { 
-    if (btnValue === '=' && output !== '') {
+// Define fuction to calculate based on button clicked.
+const calculate=(btnValue)=>{ 
+  if (btnValue === '=' && output !== '') {
         //if output has '&', replace with '/100' before evaluating.
-        output = eval(output.replace('&', '/100'));
-    } else if (btnValue === 'AC') {
-        output = ''
-    } else if (btnValue === 'DE') {
+    output = eval(output.replace('&', '/100'));
+  } else if (btnValue === 'AC') {
+    output = ''
+  } else if (btnValue === 'DE') {
         //if DE button is clicked, remove the last character from the output.
-        output = output.toString().slice(0, -1)
-    }else{
+    output = output.toString().slice(0, -1)
+  }else{
         //if output is empty and button is specialChars then return
-        if(output === '' && specialChars.includes(btnValue)) return
-        output += btnValue
-    }
+  if(output === '' && specialChars.includes(btnValue)) return
+    output += btnValue
+  }
     display.value = output
 }
 
-//Add event listener to button, call calculate() on click
+// Add event listener to button, call calculate() on click
 buttons.forEach((button) => {
-    //Button click listener calls calculator() with dataset value as argument
-    button.addEventListener('click', (e) => calculate(e.target.dataSet.value))
+    // Button click listener calls calculator() with dataset value as argument
+  button.addEventListener('click', (e) => calculate(e.target.dataSet.value))
 })
