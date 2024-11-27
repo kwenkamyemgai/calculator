5d028f0ec275c4feb2790134a7a65710c54132f4
kwenkam yemgai <yemgai21@gmail.com>
2024-11-27 15:53:12 +0100

    edits

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -4,25 +4,25 @@ const specialChars = ['*', '/', '+', '=', '-']
 let output = ''
 
 // Define fuction to calculate based on button clicked.
-const calculate=(btnValue)=>{ 
+const calculate=(btnValue) => { 
   if (btnValue === '=' && output !== '') {
-        //if output has '&', replace with '/100' before evaluating.
-    output = eval(output.replace('&', '/100'));
+    // if output has '&', replace with '/100' before evaluating.
+    output = (output.replace('&', '/100'))
   } else if (btnValue === 'AC') {
     output = ''
   } else if (btnValue === 'DE') {
-        //if DE button is clicked, remove the last character from the output.
+    // if DE button is clicked, remove the last character from the output.
     output = output.toString().slice(0, -1)
-  }else{
-        //if output is empty and button is specialChars then return
-  if(output === '' && specialChars.includes(btnValue)) return
+  } else {
+    // if output is empty and button is specialChars then return
+    if (output === '' && specialChars.includes(btnValue)) return
     output += btnValue
   }
-    display.value = output
+  display.value = output
 }
 
 // Add event listener to button, call calculate() on click
 buttons.forEach((button) => {
-    // Button click listener calls calculator() with dataset value as argument
+  // Button click listener calls calculator() with dataset value as argument
   button.addEventListener('click', (e) => calculate(e.target.dataSet.value))
 })
